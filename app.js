bace919284710973a9ecf88c9ade44a49e13d414
Anthony Goh <anthonygohkp@gmail.com>
2024-01-30 22:08:55 -0800

    insert typo for presentation

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Welcome to Devops Demo Application test!');
+  res.send('Welcome to Devops Demo Applicatiion!');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
